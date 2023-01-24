8ca6fec0a4e0972270053964d0e452747b915cda
TravisAlanH <travis.Heidelberger@trojans.dsu.edu>
2023-01-23 21:10:23 -0600

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import "./App.css";
+const apiKey = process.env.REACT_APP_API;
 
 function App() {
+  function getData() {
+    axios
+      .get(`https://newsapi.org/v2/everything?q=Apple&from=2023-01-24&sortBy=popularity&apiKey=${apiKey}`)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getData}>Test</button>
     </div>
   );
 }
